e65b55956db4906a68008d896ce346d7a8b90acb
alectogeek <alectogeek@gmail.com>
2021-06-06 16:27:47 +0200

    working with jsons state

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         Test test = new Test();
+//        Recipe recipe = new Recipe("food recipe");
+        Recipe recipe = test.createRecipeWithCategory("recipe title", "category title");
+        test.addCategoryToRecipe(recipe, "some title");
+//        Category category = new Category("food category");
+        System.out.println("Main finished");
     }
 }
